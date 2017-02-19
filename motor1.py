d04462eadaf5f828cd1c3dd77a697082bf80f0ac
bellachip49 <krystal.m.kwan@gmail.com>
2017-02-19 13:37:36 -0800

    make motor1.py

---
+++ motor1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+#Motor1A = 33
+Motor1A = 11
+#Motor1B = 35
+Motor1B = 13
+#Motor1E = 37
+Motor1E = 15
+ 
+#Motor2A = 36
+Motor2A = 22
+#Motor2B = 22
+Motor2B = 18
+#Motor2E = 32
+Motor2E = 16
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+ 
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
+ 
+print "Going forwards"
+ 
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1E,GPIO.HIGH)
+GPIO.output(Motor2E,GPIO.HIGH)
+ 
+sleep(2)
+ 
+print "Going backwards"
+ 
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.HIGH)
+GPIO.output(Motor1E,GPIO.HIGH)
+GPIO.output(Motor2E,GPIO.HIGH)
+ 
+sleep(2)
+ 
+print "Now stop"
+GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.LOW)
+ 
+GPIO.cleanup()
